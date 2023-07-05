742260ca93ad9c2aa54fad5dcaaec48211a0c462
Pablo Andres Ramirez Lopez <pabloa1311@gmail.com>
2023-07-05 12:48:31 -0500

    React Router Dom configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
-import Specials from './components/Specials';
-import Testimonial from './components/Testimonial';
+import "./App.css";
+import BookingForm from "./components/BookingForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Specials from "./components/Specials";
+import Testimonial from "./components/Testimonial";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Main></Main>
+        <Specials />
+        <Testimonial />
+      </>
+    ),
+    errorElement: <h1>Error</h1>
+  },
+  {
+    path: "/booking",
+    element: (
+      <BookingForm />
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className='app'>
-      <Header /> 
-      <Main></Main>
-      <Specials />
-      <Testimonial />
-      <Footer></Footer>
+    <div className="app">
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
     </div>
   );
 }
